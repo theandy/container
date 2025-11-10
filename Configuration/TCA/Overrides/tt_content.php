5907460911286653da6b222903c733581b562854
andreasloewer <info@andreasloewer.de>
2025-11-10 21:16:38 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,21 +5,21 @@ defined('TYPO3') or die();
 $fields = [
     'tx_backend_bgcolor' => [
         'label' => 'Hintergrundfarbe (nur Backend)',
-        'exclude' => 1,
+        'exclude' => 0, // zum Testen auf 0, später ggf. 1 + Rechte vergeben
         'config' => [
             'type' => 'input',
             'renderType' => 'colorpicker',
             'eval' => 'trim',
+            'size' => 10,
         ],
     ],
 ];
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $fields);
 
-// deine Container-CTypes eintragen
-$containerTypes = ['container_2col','container_3col'];
+// nur beim Container-Datensatz anzeigen
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content',
     'tx_backend_bgcolor',
-    implode(',', $containerTypes),
+    'container',
     'after:header'
 );
