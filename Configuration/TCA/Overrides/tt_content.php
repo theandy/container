4898f4fba133395fc917699c1fa0ce4ad87e406b
andreasloewer <info@andreasloewer.de>
2025-11-11 20:07:14 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -18,73 +18,56 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
             'ctype' => 'container_one_column',
             'label' => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c1.title',
             'desc'  => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c1.desc',
-            'grid'  => [[[ 'name' => 'content', 'colPos' => 201 ]]],
+            'grid'  => [[['name' => 'content', 'colPos' => 201]]],
             'icon'  => 'content-container-1col',
-            'group' => 'containers',
-            'wizard'=> true,
-            // Dummy, echte DS kommt per Event-Listener
-            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerDummy.xml',
+            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerCommon_1col.xml',
         ],
         [
             'ctype' => 'container_two_columns',
             'label' => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c2.title',
             'desc'  => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c2.desc',
             'grid'  => [[
-                [ 'name' => 'left',  'colPos' => 211 ],
-                [ 'name' => 'right', 'colPos' => 212 ],
+                ['name' => 'left',  'colPos' => 211],
+                ['name' => 'right', 'colPos' => 212],
             ]],
             'icon'  => 'content-container-2col',
-            'group' => 'containers',
-            'wizard'=> true,
-            // Dummy, echte DS kommt per Event-Listener
-            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerDummy.xml',
+            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerCommon_2col.xml',
         ],
         [
             'ctype' => 'container_three_columns',
             'label' => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c3.title',
             'desc'  => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:c3.desc',
             'grid'  => [[
-                [ 'name' => 'left',   'colPos' => 221 ],
-                [ 'name' => 'middle', 'colPos' => 222 ],
-                [ 'name' => 'right',  'colPos' => 223 ],
+                ['name' => 'left',   'colPos' => 221],
+                ['name' => 'middle', 'colPos' => 222],
+                ['name' => 'right',  'colPos' => 223],
             ]],
             'icon'  => 'content-container-3col',
-            'group' => 'containers',
-            'wizard'=> true,
-            // Dummy, echte DS kommt per Event-Listener
-            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerDummy.xml',
+            'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerCommon_3col.xml',
         ],
         [
             'ctype' => 'container_section',
             'label' => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:section.title',
             'desc'  => 'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:section.desc',
-            'grid'  => [[[ 'name' => 'section', 'colPos' => 231 ]]],
+            'grid'  => [[['name' => 'section', 'colPos' => 231]]],
             'icon'  => 'content-container-section',
-            'group' => 'containers',
-            'wizard'=> true,
-            // Section behält eigene XML
             'flex'  => 'FILE:EXT:container_package/Configuration/FlexForms/ContainerSection.xml',
         ],
     ];
 
     foreach ($containers as $c) {
         $config = new ContainerConfiguration($c['ctype'], $c['label'], $c['desc'], $c['grid']);
-        $config
-            ->setIcon($c['icon'])
-            ->setGroup($c['group'])
-            ->setRegisterInNewContentElementWizard((bool)$c['wizard']);
+        $config->setIcon($c['icon'])->setGroup('containers')->setRegisterInNewContentElementWizard(true);
         $registry->configureContainer($config);
 
-        if (!empty($c['flex'])) {
-            // FlexForm-DS zuordnen (leer für list_type, CType als dritter Param)
-            ExtensionManagementUtility::addPiFlexFormValue('', $c['flex'], $c['ctype']);
+        // FlexForm je CType zuordnen
+        ExtensionManagementUtility::addPiFlexFormValue('', $c['flex'], $c['ctype']);
 
-            // Tab „Container-Settings“ + Feld anhängen, ohne showitem zu überschreiben
-            ExtensionManagementUtility::addToAllTCAtypes(
-                'tt_content',
-                '--div--;LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:tab.containerSettings,pi_flexform',
-                $c['ctype']
-            );
-        }
+        // FlexForm in eigenen Tab „Container-Settings“ anhängen
+        ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            '--div--;LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:tab.containerSettings,pi_flexform',
+            $c['ctype']
+        );
     }
 })();
