ce935c6451866fc9cf064f70e6b19936c0b8fc33
andreasloewer <info@andreasloewer.de>
2025-11-11 11:19:23 +0100

    none

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -14,19 +14,16 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
     // Container "Dreispalter"
     $config = new ContainerConfiguration(
-        'container_three_columns', // CType
+        'container_three_columns',
         'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:container_three_columns.title',
         'LLL:EXT:container_package/Resources/Private/Language/locallang_db.xlf:container_three_columns.description',
-        [
-            [
-                ['name' => 'left',   'colPos' => 201],
-                ['name' => 'middle', 'colPos' => 202],
-                ['name' => 'right',  'colPos' => 203],
-            ],
-        ]
+        [[
+            ['name' => 'left',   'colPos' => 201],
+            ['name' => 'middle', 'colPos' => 202],
+            ['name' => 'right',  'colPos' => 203],
+        ]]
     );
 
-    // Icon + Wizard
     $config
         ->setIcon('content-container-3col')
         ->setGroup('containers')
@@ -34,15 +31,18 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
     $registry->configureContainer($config);
 
-    // FlexForm zuordnen
+    // FlexForm DS zuordnen
     ExtensionManagementUtility::addPiFlexFormValue(
         'container_three_columns',
         'FILE:EXT:container_package/Configuration/FlexForms/ContainerThreeColumns.xml'
     );
 
-    // FlexForm im Bearbeitungsformular sichtbar machen (eigener "Plugin"-Tab anhängen)
-    $typeKey = 'container_three_columns';
-    $GLOBALS['TCA']['tt_content']['types'][$typeKey]['showitem'] =
-        trim(($GLOBALS['TCA']['tt_content']['types'][$typeKey]['showitem'] ?? '') . ',
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:plugin,pi_flexform');
+    // FlexForm-Feld anhängen, ohne showitem zu überschreiben
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        // eigener Tab + Feld
+        '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:plugin,pi_flexform',
+        'container_three_columns',
+        'after:subheader'
+    );
 })();
